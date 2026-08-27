0882b54cdfcb18de1113cc953846c6f019309a30
helena <tech@helena-heil.de>
2026-08-27 19:51:06 +0200

    change .gitignore

---
+++ library/config.py
@@ -0,0 +1,12 @@
+WIFI_SSID = "Name"
+WIFI_PASSWORD = "Passwort"
+
+BASE_URL = "https://example.com"
+USERNAME = "Benutzername"
+PASSWORD = "Passwort"
+
+CALENDARS = [
+    "name_1",
+    "name_2",
+    "name_3"
+]
